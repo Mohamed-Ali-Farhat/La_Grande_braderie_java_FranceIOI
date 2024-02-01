7f8ccf7de597908baeef3d015b505a1b926842e5
Mohamed Ali Farhat <100702507+MohamedAliFathat@users.noreply.github.com>
2024-02-01 14:50:56 +0000

    solution

---
+++ La_Grande_Braderie.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main {
+    static Scanner entrée = new Scanner(System.in);
+    public static void main(String[] args) {
+        int a= entrée.nextInt();
+        int b= entrée.nextInt();
+        int c= entrée.nextInt();
+ 
+ System.out.println(a);
+for (int i=1 ; i<=c;i++){
+ 
+ a=a+b;
+ System.out.println(a);
+
+}
+      
+}
+
+}
